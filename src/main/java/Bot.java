f171d4fe07d20e860c993f18859f6f01e264daf7
oliszymanski <oliwier.szymanski112@gmail.com>
2021-12-03 14:52:07 +0100

    ..

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -5,7 +5,6 @@ import command.commands.ShowRolesCommand;
 import command.commands.WarnCommand;
 import listeners.MessageListener;
 
-import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.*;
 import net.dv8tion.jda.api.requests.GatewayIntent;
