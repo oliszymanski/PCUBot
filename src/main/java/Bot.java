de42b8397855153a324b0386f6b4822d4a0a475d
oliszymanski <oliwier.szymanski112@gmail.com>
2021-12-03 14:08:43 +0100

    trying to update main class

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -5,28 +5,32 @@ import command.commands.ShowRolesCommand;
 import command.commands.WarnCommand;
 import listeners.MessageListener;
 
+import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.*;
 import net.dv8tion.jda.api.requests.GatewayIntent;
+import org.jetbrains.annotations.NotNull;
 
 import javax.security.auth.login.LoginException;
 
 public class Bot {
-    public static void main(String[] args) throws LoginException {
+    public static void main(String @NotNull [] args) throws LoginException {
         if (args.length < 2) {
             System.out.println("Please ensure all arguments have been specified.");
             System.exit(1);
         }
 
         CommandSystem commandSystem = CommandSystem.createSystem(args[1])
+
+                // bot commands that can be used by authorised users
                 .addCommand(new ShowRolesCommand())
                 .addCommand(new WarnCommand())
                 .addCommand(new HelpCommand())
                 .addCommand(new SetRoleCommand());
 
-        JDABuilder.createLight(args[0], GatewayIntent.GUILD_MESSAGES)
+        JDABuilder.createLight(args[0], GatewayIntent.GUILD_MESSAGES)       // main builder
                 .addEventListeners(new MessageListener(commandSystem))
-                .setActivity(Activity.playing("with your mom"))
+                .setActivity(Activity.playing("Creating a bot"))
                 .build();
     }
 }
